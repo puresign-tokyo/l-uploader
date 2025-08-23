faf5c238a2fe6b451e5a93a356aa35630966ed4f
wefmaika <wefmaika1985@gmail.com>
2025-08-23 09:45:37 +0900

    リリースノートを上書き

--- frontend/app/composables/ReleaseNotes.ts
+++ frontend/app/composables/ReleaseNotes.ts
@@ -2,9 +2,9 @@ export function Releases() {
   // 要素番号が小さいものほど最新であるようにすること
   const releases = [
     {
-      version: "0.9.0",
-      date: "2025-04-19",
-      changes: ["ざっくり作成"],
+      version: "1.0.0",
+      date: "2025-08-23",
+      changes: ["それっぽく公開"],
     },
   ];
   return releases;
