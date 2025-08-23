41e9491c5f670214d77ff53398c97837d94273c8
wefmaika <wefmaika1985@gmail.com>
2025-08-23 20:56:25 +0900

    更新内容をリリースノートに記載

--- frontend/app/composables/ReleaseNotes.ts
+++ frontend/app/composables/ReleaseNotes.ts
@@ -2,6 +2,11 @@ export function Releases() {
   // 要素番号が小さいものほど最新であるようにすること
   const releases = [
     {
+      version: "1.0.1",
+      date: "2025-08-23",
+      changes: ["錦上京のスペカ名が表示されるようになった"],
+    },
+    {
       version: "1.0.0",
       date: "2025-08-23",
       changes: ["それっぽく公開"],
