78e02363f8cd41e363019feb5c34949b18aa179b
wefmaika <wefmaika1985@gmail.com>
2026-01-31 00:55:40 +0900

    backendのURLをポートを省略できる場合に省略して提供する共通ラッパを作成

---
+++ frontend/app/composables/Settings.ts
@@ -0,0 +1,11 @@
+export function BackendUrl() {
+  const config = useRuntimeConfig().public;
+  const httpProtocol = String(config.httpProtocol);
+  const backendHost = String(config.backendHost);
+  const backendPort = Number(config.backendPort);
+  const omitPort =
+    (httpProtocol === "http" && backendPort === 80) ||
+    (httpProtocol === "https" && backendPort === 443);
+
+  return `${httpProtocol}://${backendHost}${omitPort ? "" : `:${backendPort}`}`;
+}
